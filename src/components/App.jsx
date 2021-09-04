42fe65925f17da4028e500944309dcd8a160a1ae
nickbron <77615583+nickbron@users.noreply.github.com>
2021-09-04 20:11:32 +0300

    create

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+// import Profile from 'components/profile/Profile';
+// import Statistics from 'components/statistics/Statistics';
+// import FriendList from 'components/friendlist/FriendList';
+// import TransactionHistory from 'components/transactionHistory/TransactionHistory';
+// import user from '../user.json';
+// import statisticalData from '../statistical-data.json';
+// import friends from '../friends.json';
+// import transactions from '..//transactions.json';
+
+const App = () => {
+  return <div></div>;
+};
+export default App;
