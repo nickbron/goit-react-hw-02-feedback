7a2252119d952c869a96f360137e5a03c5e681b9
nickbron <77615583+nickbron@users.noreply.github.com>
2021-09-05 22:37:29 +0300

    create feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,10 @@
-// import Profile from 'components/profile/Profile';
-// import Statistics from 'components/statistics/Statistics';
-// import FriendList from 'components/friendlist/FriendList';
-// import TransactionHistory from 'components/transactionHistory/TransactionHistory';
-// import user from '../user.json';
-// import statisticalData from '../statistical-data.json';
-// import friends from '../friends.json';
-// import transactions from '..//transactions.json';
+import Feedback from './Feedback/Feedback';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Feedback />
+    </div>
+  );
 };
 export default App;
